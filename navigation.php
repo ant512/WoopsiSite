ed41214d44490044a04fe255af6bd8f820a70997
ant512 <devnull@localhost>
2010-08-31 16:11:11 +0100

    Deleted redundant pages.

--- navigation.php
+++ navigation.php
@@ -1,6 +1,6 @@
 					<ul>
 						<li><a href="news.php">News</a></li>
-						<li><a href="documentation.php">Documentation</a></li>
+						<li><a href="docs/index.html">Documentation</a></li>
 						<li><a href="http://bitbucket.org/ant512/woopsi">Source</a></li>
 						<li><a href="http://ant.simianzombie.com">Blog</a></li>
 						<li><a href="http://ant.simianzombie.com/forum">Community</a></li>
