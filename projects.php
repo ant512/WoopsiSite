d70a7cec49e6013d260b6e79ff913db0e42bea6d
ant512 <devnull@localhost>
2010-08-31 11:41:46 +0100

    Added more new pages.

---
+++ projects.php
@@ -0,0 +1,21 @@
+<html>
+	<head>
+		<title>Woopsi - Projects</title>
+		<link rel="stylesheet" type="text/css" href="styles/cssreset.css">
+		<link rel="stylesheet" type="text/css" href="styles/layout.css">
+	</head>
+
+	<body>
+		<div id="container">
+			<div id="top" class="darkblue">
+				<div id="header">
+					<img>
+					<? include("navigation.php"); ?>
+				</div>
+			</div>
+			<div id="content">
+			</div>
+			<div id="footer"></div>
+		</div>
+	</body>
+</html>
