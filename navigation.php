bab743422d8bee1c5070633ec599d6f4c4b52e12
ant512 <devnull@localhost>
2010-08-31 16:01:14 +0100

    More changes.

--- navigation.php
+++ navigation.php
@@ -1,11 +1,7 @@
 					<ul>
 						<li><a href="news.php">News</a></li>
-						<li><a href="docs/index.html">Documentation</a></li>
-						<li><a href="tutorials.php">Tutorials</a></li>
-						<li><a href="projects.php">Projects</a></li>
+						<li><a href="documentation.php">Documentation</a></li>
 						<li><a href="http://bitbucket.org/ant512/woopsi">Source</a></li>
-						<li><a href="faq.php">FAQ</a></li>
 						<li><a href="http://ant.simianzombie.com">Blog</a></li>
 						<li><a href="http://ant.simianzombie.com/forum">Community</a></li>
-						<li><a href="links.php">Links</a></li>
 					</ul>
