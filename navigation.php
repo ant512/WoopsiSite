33fad34b0775020a2d3d2c53e08c2942d280d556
ant512 <devnull@localhost>
2010-09-01 13:22:37 +0100

    Added screenshots page.

--- navigation.php
+++ navigation.php
@@ -1,6 +1,7 @@
 					<ul>
 						<li><a href="news.php">News</a></li>
 						<li><a href="docs/index.html">Documentation</a></li>
+						<li><a href="screenshots.php">Screenshots</a></li>
 						<li><a href="http://bitbucket.org/ant512/woopsi">Source</a></li>
 						<li><a href="http://ant.simianzombie.com">Blog</a></li>
 						<li><a href="http://ant.simianzombie.com/forum">Community</a></li>
---
+++ screenshots.php
@@ -0,0 +1,29 @@
+<html>
+	<head>
+		<title>Woopsi - Screenshots</title>
+		<link rel="stylesheet" type="text/css" href="styles/cssreset.css">
+		<link rel="stylesheet" type="text/css" href="styles/layout.css">
+	</head>
+
+	<body>
+		<div id="container">
+			<div id="top" class="darkblue">
+				<div id="header">
+					<a href="http://woopsi.org"><img src="img/logo.png"></a>
+					<? include("navigation.php"); ?>
+				</div>
+			</div>
+			<div id="content">
+				<img src="img/screenshots/bitmapdrawing.png">
+				<img src="img/screenshots/calendar.png">
+				<img src="img/screenshots/dimmedscreen.png">
+				<img src="img/screenshots/fire.png">
+				<img src="img/screenshots/keyboard.png">
+				<img src="img/screenshots/packedfonts.png">
+			</div>
+			<div id="footer">
+				<? include("footer.php"); ?>
+			</div>
+		</div>
+	</body>
+</html>
