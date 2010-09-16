47b4fd4a090106a522957a7e8738b21e3d51bcac
ant512 <devnull@localhost>
2010-09-16 14:13:08 +0100

    Added jquery news div.

--- index.php
+++ index.php
@@ -4,7 +4,14 @@
 		<title>Woopsi - Home</title>
 		<link rel="stylesheet" type="text/css" href="styles/cssreset.css">
 		<link rel="stylesheet" type="text/css" href="styles/layout.css">
-		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8"></meta>
+		<script src="javascript/jquery-1.4.2.min.js"></script>
+		<script>
+			$(document).ready(function() {
+				$('#container').before($('<div class="news" style="display: none">Latest news: Woopsi 0.99.5 released!</div>'));
+				$('.news').slideDown('slow');
+			});
+		</script>
 	</head>
 
 	<body>
@@ -16,9 +23,9 @@
 				</div>
 			</div>
 			<div id="content" class="download">
-				<a class="button lightgrey" href="downloads/woopsi-src-0.99.4.zip">Download Woopsi</a>
-				<a class="button lightgrey" href="downloads/woopsi-demo-0.99.4.zip">Download DS Demo</a>
-				<a class="button lightgrey" href="downloads/woopsi-sdl-xcode-0.99.4.zip">Download OSX Version</a>
+				<a class="button lightgrey" href="downloads/woopsi-src-0.99.5.zip">Download Woopsi</a>
+				<a class="button lightgrey" href="downloads/woopsi-demo-0.99.5.zip">Download DS Demo</a>
+				<a class="button lightgrey" href="downloads/woopsi-sdl-xcode-0.99.5.zip">Download OSX Version</a>
 			</div>
 			<div id="footer">
 				<? include("footer.php"); ?>
