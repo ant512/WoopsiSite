7a8b4e9d7cb5643d6837f7123b8fe4b2efd5b1c3
ant512 <devnull@localhost>
2011-07-21 11:42:52 +0100

    Updated index.

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 		<meta http-equiv="Content-Type" content="text/html;charset=utf-8"></meta>
 		<script src="javascript/jquery-1.4.2.min.js"></script>
 		<script>
-			var news = "06/04/2011: Version 1.2 released!";
+			var news = "21/07/2011: Version 1.3 released!";
 
 			$(document).ready(function() {
 				if (news != "") {
@@ -27,9 +27,9 @@
 				</div>
 			</div>
 			<div id="content" class="download">
-				<a class="button lightgrey" href="https://bitbucket.org/ant512/woopsi/downloads/woopsi-nds-1.2.zip">Download Woopsi</a>
-				<a class="button lightgrey" href="https://bitbucket.org/ant512/woopsi/downloads/woopsi-demo-1.2.zip">Download DS Demo</a>
-				<a class="button lightgrey" href="https://bitbucket.org/ant512/woopsisdlxcode/downloads/woopsi-osx1.2.zip">Download OSX Version</a>
+				<a class="button lightgrey" href="https://bitbucket.org/ant512/woopsi/downloads/woopsi-nds-1.3.zip">Download Woopsi</a>
+				<a class="button lightgrey" href="https://bitbucket.org/ant512/woopsi/downloads/woopsi-demo-1.3.zip">Download DS Demo</a>
+				<a class="button lightgrey" href="https://bitbucket.org/ant512/woopsisdlxcode/downloads/woopsi-osx1.3.zip">Download OSX Version</a>
 			</div>
 			<div id="footer">
 				<? include("footer.php"); ?>
